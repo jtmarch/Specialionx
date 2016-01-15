836405a9e73bd2f2f8a9cfa685d231ec83609bf3
jtmarch <jelle@4family.nl>
2016-01-15 13:27:32 +0100

    Added extra tests..

--- runtest.bat
+++ runtest.bat
@@ -1,3 +1,1 @@
-set CM_SYSTEM=org.fitnesse.plugins.GitScm
-
 mvn -Pfitnesse-integration integration-test -Dmaven.test.skip=true
--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
-set CM_SYSTEM=org.fitnesse.plugins.GitScm
+#set CM_SYSTEM=org.fitnesse.plugins.GitScm
+
+set webdriver.chrome.driver=D:\Opt\Specialisterren\chromedriver.exe
 
 mvn -Pfitnesse test -Dmaven.test.skip=true -U
